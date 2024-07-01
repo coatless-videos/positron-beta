1ea524b24de90ec4dc1fd505fd34452ac1c37ff9
James J Balamuta <coatless@users.noreply.github.com>
2024-06-30 21:45:41 -0700

    Create graphing-and-data.R

---
+++ r-workflow-script/graphing-and-data.R
@@ -0,0 +1,23 @@
+# Install R packages
+install.packages(c("plotly", "ggplot2", "reshape2"))
+
+# Load libraries
+library(plotly)
+library(reshape2)
+
+# Load data from the {datasets} package included with R
+data(volcano, package = "datasets")
+
+# Reshape data from wide to long
+df <- melt(volcano)
+
+# Construct graph with ggplot2
+p <- ggplot(df, aes(Var1, Var2, z= value)) +
+  stat_contour(geom="polygon",aes(fill=stat(level))) +
+  scale_fill_distiller(palette = "Spectral", direction = -1)
+
+# Graph with ggplot2
+p
+
+# Convert to plotly
+ggplotly(p)
