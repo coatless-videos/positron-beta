582039ad71ae562acc27246c13ec98a872321ea0
James J Balamuta <coatless@users.noreply.github.com>
2024-07-03 00:54:00 -0700

    Create setup.R

---
+++ copilot-using-chattr/setup.R
@@ -0,0 +1,10 @@
+# Install the remotes package
+install.packages("remotes")
+
+# Install the {chattr} R package from GitHub
+remotes::install_github("mlverse/chattr")
+
+# Initiate a chat session
+chattr::chattr_app()
+
+# Press 1 for Copilot
